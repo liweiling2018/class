32548f8fcb0027256e2e5e597dc1af817b58cfb2
liweiling2018 <13415106272@163.com>
2018-06-21 16:36:29 +0800

    class.h

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include "class.h"
+
+using namespace std;
+
+HelloClass::HelloClass()
+{
+   cout << "I'm being created." << endl;
+}
+
+HelloClass::~HelloClass()
+{
+   cout << "I'm being destroyed." << endl;
+}
+
+void HelloClass::sayHello()
+{
+   cout << "Hello Class!" << endl;
+}
---
+++ class.h
@@ -0,0 +1,18 @@
+#ifndef HELLOCLASS_H
+#define HELLOCLASS_H
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HelloClass
+{
+public:
+   HelloClass();
+   ~HelloClass();
+   void  sayHello();
+};
+
+#endif
